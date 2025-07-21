999e5a895fca7a207775f17f4800a12c90271773
Gopala Krishna <pasupuletigopalakrishna23@gmail.com>
2025-07-21 23:21:34 +0530

    working on scheduler

---
+++ Backgroundservices/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cron = require('node-cron');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+const schedule = () => {
+    cron.schedule('* * * * * *', () => {
+  console.log('running a task every second');
+ });
+}
+
+schedule();
+
+app.listen(process.env.PORT, () => {    
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
